f0e433d1ba2c31aa10814903a1dd4a4dd9422703
Valeriacf1 <92828533+Valeriacf1@users.noreply.github.com>
2024-09-12 20:45:28 +0000

    Paso 2: HTML dinamico

--- web.py
+++ web.py
@@ -16,16 +16,27 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(self.get_response().encode("utf-8"))
 
     def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
+    #elementos path 
+        P = self.url().path.split("/")[1:]
+        path = ": ".join(P)
 
+        #Aqui se extrae los datos del url
+        D = parse_qsl(self.url().query)
+        Q = [x for x in D]
+        query = ""
+        if Q:
+            # elementos query
+            q = [f"{y}: {z}" for (y, z) in Q]
+            query = ", ".join(q)
+
+            #Regresar los datos ordenados del path y query
+        return f"""         <h1> {path} {query} </h1>           
+"""
+        #Al introducir el comando "curl http://localhost:8000/Proyecto/web-uno?Autor=luis"
+        #regresara "<h1> Proyecto: web-uno Autor: luis </h1>"
 
 if __name__ == "__main__":
     print("Starting server")
+   # cambio de puerto a 8000
     server = HTTPServer(("localhost", 8000), WebRequestHandler)
     server.serve_forever()
